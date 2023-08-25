cc77451a487418c82b4886e8902a4cb7963f049a
beomsuyoo <beomsuyoo@gmail.com>
2023-08-25 13:57:40 +0900

    feat: add UserRequest model

---
+++ JavaSpring/filter/src/main/java/com/example/filter/model/UserRequest.java
@@ -0,0 +1,18 @@
+package com.example.filter.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(value= PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class UserRequest {
+    private String name;
+    private Integer age;
+    private String phoneNumber;
+}
