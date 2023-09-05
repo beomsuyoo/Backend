53b5576562e2c500811e098cd7e0e7e8d4d70fd0
beomsuyoo <beomsuyoo@gmail.com>
2023-09-06 08:59:08 +0900

    feat: add AccountApiController
    Return a test response.

--- JavaSpring/service/api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ JavaSpring/service/api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -1,11 +1,16 @@
 package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
+import org.delivery.api.account.model.AccountResponse;
+import org.delivery.api.common.api.Api;
+import org.delivery.api.common.error.UserErrorCode;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/account")
@@ -13,8 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountApiController {
     private final AccountRepository accountRepository;
     @GetMapping("")
-    public void save(){
-        var account = AccountEntity.builder().build();
-        accountRepository.save(account);
+    public Api<AccountResponse> save(){
+        var response = AccountResponse.builder()
+                .name("유범수")
+                .email("hi@gmail.com")
+                .registeredAt(LocalDateTime.now())
+                .build();
+        var str = "안녕";
+        var age = Integer.parseInt(str);
+
+        return Api.OK(response);
     }
 }
