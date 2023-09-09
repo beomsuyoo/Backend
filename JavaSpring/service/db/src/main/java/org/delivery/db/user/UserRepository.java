3b0f63f62f9398cccb2e321ae7001f37ee2a2cba
beomsuyoo <beomsuyoo@gmail.com>
2023-09-09 19:49:26 +0900

    refractor: use enum class for status

--- JavaSpring/service/db/src/main/java/org/delivery/db/user/UserRepository.java
+++ JavaSpring/service/db/src/main/java/org/delivery/db/user/UserRepository.java
@@ -1,5 +1,6 @@
 package org.delivery.db.user;
 
+import org.delivery.db.enums.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -7,8 +8,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
     //select * from user where id = ? and status = ? order by id desc limit 1
-    Optional<UserEntity>findFirstByIdAndStatusOrderByIdDesc(Long userId, String status);
+    Optional<UserEntity>findFirstByIdAndStatusOrderByIdDesc(Long userId, UserStatus status);
 
     // select * from user where email = ? and password = ? order by id desc limt 1
-    Optional<UserEntity>findFirstByEmailAndPasswordAndStatusOrderByIdDesc(String email, String password, String status);
+    Optional<UserEntity>findFirstByEmailAndPasswordAndStatusOrderByIdDesc(String email, String password, UserStatus status);
 }
