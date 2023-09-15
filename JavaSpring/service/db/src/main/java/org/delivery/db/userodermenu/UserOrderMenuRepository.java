bcf109bb103ea2178449d84497717eec5a60687c
beomsuyoo <beomsuyoo@gmail.com>
2023-09-16 00:05:15 +0900

    feat: add UserOrderMenuRepository

---
+++ JavaSpring/service/db/src/main/java/org/delivery/db/userodermenu/UserOrderMenuRepository.java
@@ -0,0 +1,6 @@
+package org.delivery.db.userodermenu;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserOrderMenuRepository extends JpaRepository<UserOrderMenuEntity, Long> {
+}
