82c652f8bb2a539fd23ba4dd4d8649f2f25b39a6
beomsuyoo <beomsuyoo@gmail.com>
2023-10-03 16:51:31 +0900

    feat: add UserOrderMenuRepository

--- JavaSpring/service/db/src/main/java/org/delivery/db/userodermenu/UserOrderMenuRepository.java
+++ JavaSpring/service/db/src/main/java/org/delivery/db/userodermenu/UserOrderMenuRepository.java
@@ -1,6 +1,12 @@
 package org.delivery.db.userodermenu;
 
+import org.delivery.db.userodermenu.enums.UserOrderMenuStatus;
+import org.delivery.db.userorder.enums.UserOrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserOrderMenuRepository extends JpaRepository<UserOrderMenuEntity, Long> {
+    // select * from user_order_menu where userorderid = ? and status = ?
+    List<UserOrderMenuEntity> findAllByUserOrderIdAndStatus(Long userOrderId, UserOrderStatus status);
 }
