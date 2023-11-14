ee80112dcb40ccab4c2fc7c75951e160a17eb41a
beomsuyoo <beomsuyoo@gmail.com>
2023-11-14 14:01:03 +0900

    feat: add configuration for spring security

---
+++ JavaSpring/service/store-admin/src/main/java/org/delivery/storeadmin/config/security/SecurityJpa.java
@@ -0,0 +1,47 @@
+package org.delivery.storeadmin.config.security;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityJpa {
+    private final List<String> SWAGGER = List.of(
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    );
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf().disable()
+                .authorizeHttpRequests(it->{
+                    it
+                            .requestMatchers(
+                                    PathRequest.toStaticResources().atCommonLocations()
+                            ).permitAll() // permit all static resources
+                            .mvcMatchers(
+                                    SWAGGER.toArray(new String[0])
+                            ).permitAll() // permit swagger
+
+                            .mvcMatchers(
+                                    "/open-api.**"
+                            ).permitAll() // permit open-api
+
+                            .anyRequest().authenticated() // others authenticated
+                            ;
+
+                })
+                .formLogin(Customizer.withDefaults())
+                ;
+        return httpSecurity.build();
+
+    }
+}
