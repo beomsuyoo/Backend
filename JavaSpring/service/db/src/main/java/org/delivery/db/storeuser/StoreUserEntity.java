c5fa64f4109b6517669f34921a3d7838af295498
beomsuyoo <beomsuyoo@gmail.com>
2023-11-14 14:00:14 +0900

    feat: add StoreUserEntity

--- JavaSpring/service/db/src/main/java/org/delivery/db/storeuser/StoreUserEntity.java
+++ JavaSpring/service/db/src/main/java/org/delivery/db/storeuser/StoreUserEntity.java
@@ -7,9 +7,12 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.delivery.db.BaseEntity;
 import org.delivery.db.storemenu.enums.StoreMenuStatus;
+import org.delivery.db.storeuser.enums.StoreUserRole;
+import org.delivery.db.storeuser.enums.StoreUserStatus;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -17,7 +20,7 @@ import java.math.BigDecimal;
 @SuperBuilder
 @EqualsAndHashCode(callSuper = true)
 @Entity
-@Table(name = "store_store")
+@Table(name = "store_user")
 public class StoreUserEntity extends BaseEntity {
 
     @Column(nullable = false)
@@ -33,6 +36,14 @@ public class StoreUserEntity extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private StoreUserStatus status;
 
-    @Column(length= 100, nullable = false)
-    private String role;
+    @Column(length= 50, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StoreUserRole role;
+
+    private LocalDateTime registeredAt;
+
+    private LocalDateTime unregisteredAt;
+
+    private LocalDateTime lastLoginAt;
+
 }
