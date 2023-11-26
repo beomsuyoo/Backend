538156b2f091535708d5106750883574d3289ee8
beomsuyoo <beomsuyoo@gmail.com>
2023-11-26 12:15:14 +0900

    feat: add sql query

--- JavaSpring/service/db/src/main/java/org/delivery/db/store/StoreRepository.java
+++ JavaSpring/service/db/src/main/java/org/delivery/db/store/StoreRepository.java
@@ -17,4 +17,7 @@ public interface StoreRepository extends JpaRepository<StoreEntity,Long> {
 
     // valid category store list
     List<StoreEntity> findAllByStatusAndCategoryOrderByStarDesc(StoreStatus status, StoreCategory category);
+
+    // select * from store where name = ? and status = ? order by id desc limit 1
+    Optional<StoreEntity> findFirstByNameAndStatusOrderByIdDesc(String name, StoreStatus status);
 }
